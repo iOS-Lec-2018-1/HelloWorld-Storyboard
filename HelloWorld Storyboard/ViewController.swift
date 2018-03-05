ee53ff3256ed47e874ca931a74db9468fda0e1fd
김종현 <jhkim@dit.ac.kr>
2018-03-05 22:46:06 +0900

    add action event

--- HelloWorld Storyboard/ViewController.swift
+++ HelloWorld Storyboard/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("Button pressed!!")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
